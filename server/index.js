8ca6845825280ba479351af8b39c1a33d322ba8d
Shams Tanveer Jim <shamstanveer@iut-dhaka.edu>
2023-03-18 15:18:15 +0600

    Server Commit 2

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const connectDb = require("./config/dbConnection");
 const errorHanlde = require("./middleWare/errorHandler");
 const port = process.env.PORT || 5000;
 connectDb();
 app.use(express.json());
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    method: "GET,POST,PUT,UPDATE,DELETE",
+    credentials: true
+}))
+
 app.use("/user",require("./routes/userRoute"));
 app.use(errorHanlde);
+
+
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const {registerUser,getUserByEmail,updateUserByEmail,deleteUserByEmail} = require("../controller/userController");
+const { getJWT } = require("../jwt/jwtAuth");
 const validateToken = require("../middleWare/validateTokenHandle");
 
 router.post("/register",registerUser);
 router.delete("/delete",validateToken,deleteUserByEmail);
 router.put("/update",validateToken,updateUserByEmail);
 router.get("/profile",validateToken, getUserByEmail);
-router.get("/message",(req,res)=>{
-    res.send("hi");
+router.get("/jwt",(req,res)=>{
+   const accessToken =  getJWT(req.params.email);
+   res.status(200).json({accessToken:accessToken});
 });
 module.exports = router;
