b81e02edb5916a6d047bc61c5326bcf45554e915
Shams Tanveer Jim <shamstanveer@iut-dhaka.edu>
2023-03-18 10:22:05 +0600

    Client Commit

--- server/security/encryption.js
+++ server/security/encryption.js
@@ -8,25 +8,47 @@ const initVector = crypto
     .update(process.env.SECRET_IV)
     .digest('hex')
     .substring(0, 16);
-const message = "This is a secret message";
-const Securitykey = crypto
+
+const securitykey = crypto
     .createHash('sha512')
     .update(process.env.SECRET_KEY)
     .digest('hex')
     .substring(0, 32);
 
-const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
 
-let encryptedData = cipher.update(message, "utf-8", "hex");
 
-encryptedData += cipher.final("hex");
 
-console.log("Encrypted message: " + encryptedData);
 
-const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+const encryptData = (data)=> {
+    const cipher = crypto.createCipheriv(algorithm, securitykey, initVector)
+    return Buffer.from(
+        cipher.update(data, 'utf8', 'hex') + cipher.final('hex')
+    ).toString('base64')
+}
 
-let decryptedData = decipher.update(encryptedData, "hex", "utf-8");
+const decryptData = (encryptedData)=> {
+    const buff = Buffer.from(encryptedData, 'base64')
+    const decipher = crypto.createDecipheriv(algorithm, securitykey, initVector)
+    return (
+        decipher.update(buff.toString('utf8'), 'hex', 'utf8') +
+        decipher.final('utf8')
+    )
+}
 
-decryptedData += decipher.final("utf8");
+module.exports = {encryptData,decryptData};
 
-console.log("Decrypted message: " + decryptedData);
+//onst cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+//
+//et encryptedData = cipher.update(message, "utf-8", "hex");
+//
+//ncryptedData += cipher.final("hex");
+//
+//onsole.log("Encrypted message: " + encryptedData);
+//
+//onst decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
+//
+//et decryptedData = decipher.update(encryptedData, "hex", "utf-8");
+//
+//ecryptedData += decipher.final("utf8");
+//
+//onsole.log("Decrypted message: " + decryptedData);
