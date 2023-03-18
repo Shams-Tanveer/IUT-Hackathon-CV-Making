1f3c89287cdc251c036c56a8c3a80d465d6d1a2a
Shams Tanveer Jim <shamstanveer@iut-dhaka.edu>
2023-03-18 15:41:58 +0600

    Server Commit 4

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -8,6 +8,9 @@ router.post("/register",registerUser);
 router.delete("/delete",validateToken,deleteUserByEmail);
 router.put("/update",validateToken,updateUserByEmail);
 router.get("/profile", getUserByEmail);
+router.get("/message", (req,res)=>{
+    res.send("Hi")
+});
 router.get("/jwt",(req,res)=>{
    const accessToken =  getJWT(req.params.email);
    res.status(200).json({accessToken:accessToken});
